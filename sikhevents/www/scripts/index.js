d57ce1a884c2736bfb4f6cbaff82c4edb7219bb2
vikramsingh <vikramkhalsa@outlook.com>
2017-01-27 11:06:07 -0800

    new project , added jquery and started adding customizing for sikhevents

--- sikhevents/www/scripts/index.js
+++ sikhevents/www/scripts/index.js
@@ -13,11 +13,41 @@
         document.addEventListener( 'resume', onResume.bind( this ), false );
         
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        //var parentElement = document.getElementById('deviceready');
+        //var listeningElement = parentElement.querySelector('.listening');
+        //var receivedElement = parentElement.querySelector('.received');
+        //listeningElement.setAttribute('style', 'display:none;');
+        //receivedElement.setAttribute('style', 'display:block;');
+
+        //if (navigator.connection.type === Connection.NONE) {
+        //    navigator.notification.alert('An internet connection is required to continue');
+        //} else {
+        //    window.location = "http://www.sikh.events";
+        //}
+        //var targetUrl = "http://www.sikh.events";
+        //var bkpLink = document.getElementById("bkpLink");
+        //bkpLink.setAttribute("href", targetUrl);
+        //bkpLink.text = targetUrl;
+        //window.location.replace(targetUrl);
+        $.getJSON("http://www.sikh.events/getprograms.php", function (data) {
+
+            console.log("testing");
+            var items = [];
+            $.each( data, function( key, val ) {
+                items.push(
+                    "<div class = \"cell\"><br> " +val["title"] +
+                    "<br>" + val["description"] +
+                    "<br>" + val["subtitle"] +
+                   "<br>" + val["sd"] +
+                    "</div>"
+                    )
+            });
+ 
+            $( "<ul/>", {
+                "class": "my-new-list",
+                html: items.join( "" )
+            }).appendTo( "body" );
+        });
     };
 
     function onPause() {
