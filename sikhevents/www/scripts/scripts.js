b4f3f5b1bde69fc7c2c04d76def44b753f834004
vikramsingh <vikramkhalsa@outlook.com>
2017-03-15 12:22:10 -0700

    updated date parsing/formatting to (hopefully) support all platforms. Previous parsing had issues on iOS.

--- sikhevents/www/scripts/scripts.js
+++ sikhevents/www/scripts/scripts.js
@@ -34,17 +34,28 @@ function showAbout() {
 }
 
 function formatDate(d) {
-    var dt = new Date(d);
-    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-    var day = days[dt.getDay()];
-    var date = dt.getDate();
-    var month = months[dt.getMonth()];
-    var datestr = month + " " + date;
-    var hrs = dt.getHours();
-    var ampm = hrs < 12 ? "am" : "pm";
-    hrs = hrs > 12 ? hrs - 12 : hrs == 0 ? 12 : hrs;
-    var mins = dt.getMinutes();
-    var timestr = hrs + ":" + mins + " " + ampm;
-    return [day, datestr, timestr];
+    try
+    {
+        //var dt = new Date(d);
+        var dateArr = d.split(' ');
+        var date1 = dateArr[0].split('-');
+        var time1 = dateArr[1].split(':');
+
+        var dt = new Date(date1[0], date1[1], date1[2], time1[0], time1[1], time1[2]);
+
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+        var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        var day = days[dt.getDay()];
+        var date = dt.getDate();
+        var month = months[dt.getMonth()];
+        var datestr = month + " " + date;
+        var hrs = dt.getHours();
+        var ampm = hrs < 12 ? "am" : "pm";
+        hrs = hrs > 12 ? hrs - 12 : hrs == 0 ? 12 : hrs;
+        var mins = dt.getMinutes();
+        var timestr = hrs + ":" + mins + " " + ampm;
+        return [day, datestr, timestr];
+    } catch (ex) {
+        return d; //if parsing/formatting failed, just show original date as is
+    }
 }
